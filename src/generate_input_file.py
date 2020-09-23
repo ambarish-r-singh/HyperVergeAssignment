095ecda2b1a2339f70dba1391717e040d41f71b1
ambarish.singh <ambarish.r.singh@gmail.com>
2020-09-24 02:27:49 +0530

    24 Sept - formatting and beautification

--- src/generate_input_file.py
+++ src/generate_input_file.py
@@ -10,19 +10,23 @@ def natural_sort_key(s):
 
 
 # Generate input File to write the extracted car number text
-inputFileDirectory = "C:\\Users\\ambarish.singh\\PycharmProjects\\HyperVergeAssignment\\input\\"
-inputFileName = inputFileDirectory + "inputFile.txt"
-if os.path.exists(inputFileName):
-    os.remove(inputFileName)
-inputFile = open(inputFileName, "a")
-inputFile.write("filename,expected\n")
+def generate_input_file():
+    input_file_directory = "C:\\Users\\ambarish.singh\\PycharmProjects\\HyperVergeAssignment\\input\\"
+    input_file_name = input_file_directory + "inputFile.txt"
+    if os.path.exists(input_file_name):
+        os.remove(input_file_name)
+    input_file = open(input_file_name, "a")
+    input_file.write("filename,expected\n")
 
-# Traverse through the directory containing the text files.
-directory = "C:\\Users\\ambarish.singh\\PycharmProjects\\HyperVergeAssignment\\input\\groundtruth\\"
-fileList = os.listdir(directory)
-fileList.sort(key=natural_sort_key)
-for filename in fileList:
-    if filename.endswith(".txt"):
-        f = open(directory + filename)
-        lines = f.read()
-        inputFile.write(filename.replace(".txt", ".jpg") + "," + lines.strip() + "\n")
+    # Traverse through the directory containing the text files.
+    directory = "C:\\Users\\ambarish.singh\\PycharmProjects\\HyperVergeAssignment\\input\\groundtruth\\"
+    file_list = os.listdir(directory)
+    file_list.sort(key=natural_sort_key)
+    for filename in file_list:
+        if filename.endswith(".txt"):
+            f = open(directory + filename)
+            lines = f.read()
+            input_file.write(filename.replace(".txt", ".jpg") + "," + lines.strip() + "\n")
+
+
+pass
